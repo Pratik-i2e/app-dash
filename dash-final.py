012cfc6a9115a642895724e80576c7bac7e5b666
Pratik-i2e <pratik.tabhane@i2econsulting.com>
2025-06-02 16:09:40 +0530

    Update dash-final.py

--- dash-final.py
+++ dash-final.py
@@ -9,7 +9,7 @@ import datetime
 import os
 
 # --- Set Groq API credentials ---
-groq_api_key = "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"
+groq_api_key = SECRET_GROQ
 groq_api_base = "https://api.groq.com/openai/v1"
 model_name = "llama3-70b-8192"
 
