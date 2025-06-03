7eeb377baa6901515b2be787d8ac8251b996617c
Pratik-i2e <pratik.tabhane@i2econsulting.com>
2025-06-03 20:53:25 +0530

    Update dash-final.py

--- dash-final.py
+++ dash-final.py
@@ -156,7 +156,7 @@
 
 import('pysqlite3')
 import sys
-sys.modules[‘sqlite3’] = sys.modules.pop(‘pysqlite3’)
+sys.modules['sqlite3'] = sys.modules.pop(‘pysqlite3’)
 import streamlit as st
 import pandas as pd
 import os
